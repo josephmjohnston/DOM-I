26ea44a45bf553fb74d9d953c9e60c02b26b0073
josephmjohnston <josephmjohnston9985@gmail.com>
2020-06-03 18:34:17 -0500

    struggling on nav

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const siteContent = {
+
   "nav": {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
@@ -38,5 +39,112 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.src = siteContent.nav["img-src"]
+
+let navItems = document.querySelectorAll('header nav a')
+// for(let i = 0; i < navItems.length; i++){
+//   navItems[i].innerText = siteContent.nav["nav-items-"];
+// }
+// navItems.forEach()
+navItems[0].innerText = siteContent.nav["nav-item-1"]
+navItems[1].innerText = siteContent.nav["nav-item-2"]
+navItems[2].innerText = siteContent.nav["nav-item-3"]
+navItems[3].innerText = siteContent.nav["nav-item-4"]
+navItems[4].innerText = siteContent.nav["nav-item-5"]
+navItems[5].innerText = siteContent.nav["nav-item-6"]
+
+let home = document.createElement('a')
+let github = document.createElement('a')
+
+let mainNav = document.querySelector('header nav')
+
+mainNav.prepend(home);
+mainNav.appendChild(github);
+
+home.innerText = 'Home'
+github.innerText = 'Github'
+home.href = '#'
+github.href = '#'
+
+navItems[0].style.color = 'green'
+navItems[1].style.color = 'green'
+navItems[2].style.color = 'green'
+navItems[3].style.color = 'green'
+navItems[4].style.color = 'green'
+navItems[5].style.color = 'green'
+
+
+
+
+
+let ctaText = document.querySelector('.cta .cta-text h1')
+ctaText.innerText = siteContent.cta["h1"]
+ctaText.style.color = 'red'
+
+let ctaButton = document.querySelector('.cta .cta-text button')
+ctaButton.innerText = siteContent.cta.button
+
+let ctaImage = document.querySelector('#cta-img')
+ctaImage.src = siteContent.cta["img-src"]
+
+
+
+
+
+let topContentHeaders = document.querySelectorAll('.top-content .text-content h4')
+topContentHeaders[0].innerText = siteContent["main-content"]["features-h4"]
+topContentHeaders[0].style.color = 'red'
+topContentHeaders[1].innerText = siteContent["main-content"]["about-h4"]
+topContentHeaders[1].style.color = 'red'
+
+let topContentText = document.querySelectorAll('.top-content .text-content p')
+topContentText[0].innerText = siteContent["main-content"]["features-content"]
+topContentText[1].innerText = siteContent["main-content"]["about-content"]
+
+
+
+
+
+let middleImage = document.querySelector('.middle-img')
+middleImage.src = siteContent["main-content"]["middle-img-src"]
+
+
+
+
+
+let bottomContentHeaders = document.querySelectorAll('.bottom-content .text-content h4')
+bottomContentHeaders[0].innerText = siteContent["main-content"]["services-h4"]
+bottomContentHeaders[0].style.color = 'red'
+bottomContentHeaders[1].innerText = siteContent["main-content"]["product-h4"]
+bottomContentHeaders[1].style.color = 'red'
+bottomContentHeaders[2].innerText = siteContent["main-content"]["vision-h4"]
+bottomContentHeaders[2].style.color = 'red'
+
+let bottomContentText = document.querySelectorAll('.bottom-content .text-content p')
+bottomContentText[0].innerText = siteContent["main-content"]["services-content"]
+bottomContentText[1].innerText = siteContent["main-content"]["product-content"]
+bottomContentText[2].innerText = siteContent["main-content"]["vision-content"]
+
+
+
+
+
+// let contactHeader = document.querySelectorAll('.content h4')
+let contactHeader = document.querySelector('.contact h4')
+contactHeader.innerText = siteContent.contact["contact-h4"]
+contactHeader.style.color = 'red'
+
+let contactText = document.querySelectorAll('.contact p')
+contactText[0].innerText = siteContent.contact.address
+contactText[1].innerText = siteContent.contact.phone
+contactText[2].innerText = siteContent.contact.email
+
+
+
+
+
+let footerText = document.querySelector('footer p')
+footerText.innerText = siteContent["footer"]["copyright"] 
